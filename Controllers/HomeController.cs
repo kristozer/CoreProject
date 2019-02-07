0b6545395c4b9d9f722367de2d47992acc041f34
Юнак Андрей <kristall@empl.vvsu.ru>
2019-02-07 12:08:08 +1000

    добавленафильтрация сортировака и пагинация

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,11 +23,57 @@ namespace CoreProject.Controllers
         {
             return View();
         }
-
-        public ActionResult GetUsers(int? company, string name)
+        [HttpGet]
+        public async Task<IActionResult> GetUsers(int? company, string name, int page = 1,
+                    SortState sortOrder = SortState.NameAsc)
         {
-            
-                return View();
+            int pageSize = 3;
+
+            IQueryable<User> users = db.Users.Include(p => p.CompanyId == company);
+            if (company != null && company != 0)
+            {
+                users = users.Where(p => p.CompanyId == company);
+            }
+            if (!String.IsNullOrEmpty(name))
+            {
+                users = users.Where(p => p.Name.Contains(name));
+            }
+
+            switch (sortOrder)
+            {
+                case SortState.NameDesc:
+                    users = users.OrderByDescending(s => s.Name);
+                    break;
+                case SortState.AgeAsc:
+                    users = users.OrderBy(s => s.Age);
+                    break;
+                case SortState.AgeDesc:
+                    users = users.OrderByDescending(s => s.Age);
+                    break;
+                case SortState.CompanyAsc:
+                    users = users.OrderBy(s => s.Company.Name);
+                    break;
+                case SortState.CompanyDesc:
+                    users = users.OrderByDescending(s => s.Company.Name);
+                    break;
+                default:
+                    users = users.OrderBy(s => s.Name);
+                    break;
+            }
+
+            // пагинация
+            var count = await users.CountAsync();
+            var items = await users.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            // формируем модель представления
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                PageViewModel = new PageViewModel(count, page, pageSize),
+                SortViewModel = new SortViewModel(sortOrder),
+                FilterViewModel = new FilterViewModel(db.Companies.ToList(), company, name),
+                Users = items
+            };
+            return View(viewModel);
         }
         public IActionResult Privacy()
         {
---
+++ Models/FilterViewModel.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CoreProject.Models
+{
+    public class FilterViewModel
+    {
+        public FilterViewModel(List<Company> companies, int? company, string name)
+        {
+            // устанавливаем начальный элемент, который позволит выбрать всех
+            companies.Insert(0, new Company { Name = "Все", Id = 0 });
+            Companies = new SelectList(companies, "Id", "Name", company);
+            SelectedCompany = company;
+            SelectedName = name;
+        }
+        public SelectList Companies { get; private set; } // список компаний
+        public int? SelectedCompany { get; private set; }   // выбранная компания
+        public string SelectedName { get; private set; }    // введенное имя
+    }
+}
