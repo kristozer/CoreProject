303ce1bded346f4700f940d27bf5a9d9c71d9675
Юнак Андрей <kristall@empl.vvsu.ru>
2019-02-07 11:22:49 +1000

    изменение старого кода

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using CoreProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using CoreProject.Services;
+using CoreProject.Data;
 
 namespace CoreProject.Controllers
 {
@@ -18,55 +19,15 @@ namespace CoreProject.Controllers
         {
             this.db = context;
         }
-        public async Task<IActionResult> Index(SortState sortOrder = SortState.NameAsc)
+        public ActionResult Index()
         {
-            IQueryable<User> users = db.Users.Include(x => x.Company);
-
-            switch (sortOrder)
-            {
-                case SortState.NameDesc:
-                    users = users.OrderByDescending(s => s.Name);
-                    break;
-                case SortState.AgeAsc:
-                    users = users.OrderBy(s => s.Age);
-                    break;
-                case SortState.AgeDesc:
-                    users = users.OrderByDescending(s => s.Age);
-                    break;
-                case SortState.CompanyAsc:
-                    users = users.OrderBy(s => s.Company.Name);
-                    break;
-                case SortState.CompanyDesc:
-                    users = users.OrderByDescending(s => s.Company.Name);
-                    break;
-                default:
-                    users = users.OrderBy(s => s.Name);
-                    break;
-            }
-            IndexViewModel viewModel = new IndexViewModel
-            {
-                Users = await users.AsNoTracking().ToListAsync(),
-                SortViewModel = new SortViewModel(sortOrder)
-            };
-            return View(viewModel);
+            return View();
         }
 
         public ActionResult GetUsers(int? company, string name)
         {
-            IQueryable<User> users = db.Users.Include(p => p.Company);
-            if ((company ?? 0) != 0)
-                users = users.Where(p => p.CompanyId == company);
-            if (!string.IsNullOrEmpty(name))
-                users = users.Where(p => p.Name.Contains(name));
-                var companies = db.Companies.ToList();
-                companies.Insert(0, new Company{Name= "ВСЕ", Id=0});
-                UsersListViewModel viewModel = new UsersListViewModel()
-                {
-                    Users = users.ToList(),
-                    Companies = new SelectList(companies, "Id", "Name"),
-                    Name = name
-                };
-                return View(viewModel);
+            
+                return View();
         }
         public IActionResult Privacy()
         {
@@ -77,11 +38,6 @@ namespace CoreProject.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public IActionResult Person()
-        {
-            var p = new Person { Name = "TTT", Date = DateTime.Now };
-            return View(p);
         }
     }
 }
--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
+using CoreProject.Models;
+
 namespace CoreProject.Models
 {
     public class IndexViewModel
     {
         public IEnumerable<User> Users { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+        public FilterViewModel FilterViewModel { get; set; }
         public SortViewModel SortViewModel { get; set; }
     }
 }
